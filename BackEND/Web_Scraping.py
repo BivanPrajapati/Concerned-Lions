ac3a5c8b71990b36cdfcccd1ab54c7c94b412c93
alanaisi-ali <alanaisi@bu.edu>
2025-10-25 13:40:04 -0400

    made two functions, one to return the class name in an url wayie and the second is to find the url that we will use

--- BackEND/Web_Scraping.py
+++ BackEND/Web_Scraping.py
@@ -2,9 +2,26 @@ import requests
 from bs4 import BeautifulSoup
 import time
 import csv
-
+import re
 URL = "https://www.bu.edu/academics/cas/courses/"
+
 HEADERS = {
     "User-Agent": "CourseScraper/1.0 (DS-X Hackathon)"
 }
 
+def normalize_course(course_name):
+    # Convert to lowercase
+    course_name = course_name.lower()
+    course_name = re.sub(r'\b(comp\s*sci|computer\s*science)\b', 'cs', course_name)
+    course_name = re.sub(r'\s+', ' ', course_name).strip()
+    match = re.match(r'([a-z]+)\s*-?\s*(\d+)', course_name)
+    if match:
+        dept, number = match.groups()
+        return f"cas-{dept}-{number}"
+    else:
+        return None
+
+def find_class(finding_url):
+    return URL + finding_url + "/"
+
+print(find_class(normalize_course("cs112")))
