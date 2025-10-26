d1a659d59813f4c9ffed0388bb0de7acfaf893e6
wonjunni0523 <111329928+wonjunni0523@users.noreply.github.com>
2025-10-25 20:05:28 -0400

    Add files via upload
    Alan

--- FrontEnd/ConnectedLions.js
+++ FrontEnd/ConnectedLions.js
@@ -1,3 +1,12 @@
+let searchpop=document.getElementsById('search-popup')
+function Search(){
+    setInterval(drop_bar(), 1500)
+}
+function drop_bar(){
+   searchpop.style.display="block";
+}
 function Searchbar(){
     
 }
+
+
